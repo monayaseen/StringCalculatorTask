b5691b947f4a9100e0e63d8ac60968f7da3b5190
muna yassin <muna.yassin@gmail.com>
2021-08-12 01:59:20 +0300

    Initial Commit

---
+++ StringCalculatorTask.Test/StringCalculatorTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace StringCalculatorTask.Test
+{
+    public class StringCalculatorTest
+    {
+        [Fact]
+        public void EmptyString_ReturnsZero()
+        {
+            
+        }
+    }
+}
---
+++ StringCalculatorTask/StringCalculator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace StringCalculatorTask
+{
+    public class StringCalculator
+    {
+        
+        
+    }
+}
