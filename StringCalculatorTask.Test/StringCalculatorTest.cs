86a1dc0c2c2b7d442bd7c9b759c955d3f74b91c3
muna yassin <muna.yassin@gmail.com>
2021-08-12 03:13:46 +0300

    Refactor and handle an unknown amount of numbers

--- StringCalculatorTask.Test/StringCalculatorTest.cs
+++ StringCalculatorTask.Test/StringCalculatorTest.cs
@@ -13,26 +13,16 @@ namespace StringCalculatorTask.Test
         [InlineData("2",2)]
         [InlineData("1,2",3)]
         [InlineData("1,5",6)]
-        
+        [InlineData("10,9",19)]
+        [InlineData("1,3,5",9)]
+        [InlineData("1,9,3,5",18)]
+        [InlineData("17,20,3,9",49)]
         public void StringNumbers_ReturnsIntSum(string numbers,int expectedSum)
         {
             var calculator = new StringCalculator();
             var result = calculator.add(numbers);
             Assert.Equal(expectedSum,result);
         }
-        
-        [Theory]
-        [InlineData("10,9",19)]
-        [InlineData("1,3,5",9)]
-        [InlineData("1,9,3,5",18)]
-        [InlineData("17,20,3,9",49)]
-        public void MoreThanTwoStringNumbers_ReturnsIntSum(string numbers,int expectedSum)
-        {
-            var calculator = new StringCalculator();
-            var result = calculator.add(numbers);
-            Assert.Equal(expectedSum,result);
-        }
-        
-        
+
     }
 }
